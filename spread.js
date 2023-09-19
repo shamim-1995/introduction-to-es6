c9ebe392f926096881d8f7dcf579eab1e08f8a9d
shamim-1995 <sashamim987@gmail.com>
2023-09-19 19:02:54 +0600

    added spread function section

---
+++ spread.js
@@ -0,0 +1,27 @@
+const max = Math.max(16, 20, 45, 1, 89, 23)
+console.log(max);
+
+const numbers = [3, 5, 2, 45, 5, 43, 90, 32, 15];
+const arrayMax = Math.max(...numbers);
+// console.log(numbers);
+// console.log(...numbers);
+console.log(arrayMax);
+
+
+// Use spread operator or copy.
+const friends = [4, 5, 87, 9];
+const bondhu = friends;
+// bondhu.push(12);
+// console.log(friends);
+// console.log(bondhu);
+const dosto = [...friends]   // copy
+console.log(dosto);
+friends.push(100);
+console.log(dosto);
+console.log(friends);
+
+
+//Advanced..
+
+const sonkha = [...friends, 9999]; //add extra elements while copy.
+console.log(sonkha);
