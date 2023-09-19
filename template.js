bec2dbf809876c8a3f21489bc9c977e911a5d25f
shamim-1995 <sashamim987@gmail.com>
2023-09-19 13:55:24 +0600

    added template section

---
+++ template.js
@@ -0,0 +1,23 @@
+const first = 'shamim';
+const last = 'sikdar';
+const greet = 'potas';
+const name = first + ' ' + last + ' ' + greet;
+console.log(name);
+
+const a = 10;
+const b = 20;
+const result = 'your result of sum ' + a + ' and ' + b + ' is ' + (a + b);
+console.log(result);
+
+const math = `The sum of ${a} and ${b} is ${a+b}`
+console.log(math);
+
+const challenge = `shamim sikdar
+tmi naki valo 
+naki ami valo`
+console.log(challenge);
+
+const numbers = [12, 34, 90, 89];
+const student1 = {name:'shamim sikdar', age:5}
+const math2 = `The sum of ${numbers[0]} and ${student1.age} is ${numbers[0] + student1.age}`
+console.log(math2);
